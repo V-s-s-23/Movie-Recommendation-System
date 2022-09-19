9456f765b4267776e51b73db139ce7cef36b5ae7
Vinay Shanker Shukla <103623927+V-s-s-23@users.noreply.github.com>
2022-09-19 16:59:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=fbadc838a6137178493c3e515515af9a&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+
+
+movies_dict = pickle.load(open('movies_dict.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+
+
+def recommender(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_poster = []
+
+
+
+    for i in movie_list:
+        movies_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # fetch poster from api
+        recommended_movies_poster.append(fetch_poster(movies_id))
+    return recommended_movies,recommended_movies_poster
+
+st.title('Movie Recommender System')
+selected_movie = st.selectbox(
+    'How would you like to be contacted?',
+    (movies['title'].values))
+
+if st.button('Recommend'):
+    names,posters = recommender(selected_movie)
+
+    col1, col2, col3,col4,col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
